6d88a7674938ef2213f9c8871cb3494ef72212a4
lhw <triaixis159@gmail.com>
2025-01-14 16:09:14 +0900

    개발자 프로필 및 UI 개선
    프로필 사진 변경 기능 강화, 튜토리얼 커스텀 툴팁 추가, GitHub 링크 삽입 등 UX/UI 개선 작업 수행. 또한 Tech Stack에 외부 URL 추가 및 ImageWithSkeleton 컴포넌트 도입으로 이미지 로딩 경험 최적화.

--- frontend/src/lib/apiClient.js
+++ frontend/src/lib/apiClient.js
@@ -1,8 +1,14 @@
-// 기본 axios 인스턴스 설정
 import axios from "axios";
+import https from 'https';
+
+const agent = new https.Agent({
+  cert: process.env.CLIENT_CERT, // 인증서
+  key: process.env.CLIENT_KEY,   // 개인 키
+});
 
 const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL, // API의 기본 URL 설정 (NEXT_PUBLIC 이 붙어야 클라이언트에서 접근가능)
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // API의 기본 URL 설정 (NEXT_PUBLIC 이 붙어야 외부에 노출됨)
+  httpsAgent: agent,
   headers: {
     'Content-Type': 'application/json', // 요청 헤더에 JSON 타입 설정
   },
