c7e63700192dbe822fcad49e4281be04adb90a26
LeeHyunWoo2 <xroai1@naver.com>
2025-01-15 14:14:10 +0900

    Update apiClient.js

--- frontend/src/lib/apiClient.js
+++ frontend/src/lib/apiClient.js
@@ -10,16 +10,9 @@ const apiClient = axios.create({
   timeout: 10000, // 요청이 너무 오래걸리면 자동으로 실패하도록 설정
 });
 
-console.log('Cloudflare API Token:', process.env.CLOUDFLARE_API_TOKEN);
-
 // 요청 인터셉터를 통해 Vercel 서버에서 백엔드 경로 접근 제한용으로 만든 헤더 추가
 apiClient.interceptors.request.use((config) => {
   config.headers['x-api-key'] = process.env.CLOUDFLARE_API_TOKEN;
-  console.log('Axios 요청 헤더:', config.headers);
-  console.log('process.env.NEXT_PUBLIC_API_URL : ',process.env.NEXT_PUBLIC_API_URL)
-  console.log('process.env.UPLOADTHING_TOKEN : ',process.env.UPLOADTHING_TOKEN)
-  console.log('process.env.RECAPTCHA_SITE_KEY : ',process.env.RECAPTCHA_SITE_KEY)
-  console.log('process.env.CLOUDFLARE_API_TOKEN : ',process.env.CLOUDFLARE_API_TOKEN)
   return config;
 });
 
