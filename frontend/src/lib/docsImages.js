9d11c9e1cae27a406719ce273e0164dc68d5aedf
lhw <triaixis159@gmail.com>
2025-02-20 17:58:51 +0900

    태그 필터 기능 추가 및 관련 UI 개선
    태그 기반 필터링 기능과 AND/OR 모드 선택 추가. 관련된 UI 컴포넌트 및 스타일 업데이트, 노트 필터 관련 버그 수정.

--- frontend/src/lib/docsImages.js
+++ frontend/src/lib/docsImages.js
@@ -1,71 +1,86 @@
 const dimg = {
   security: {
-    https:{
-    https1: require('../../public/images/docs/security/https/https.PNG'),
-    https2: require('../../public/images/docs/security/https/https2.PNG'),
-    backendssl: require('../../public/images/docs/security/https/backend ssl blur.png'),
-    certificate: require('../../public/images/docs/security/https/인증서.PNG'),
+    https: {
+      https1: require('../../public/images/docs/security/https/https.PNG'),
+      https2: require('../../public/images/docs/security/https/https2.PNG'),
+      backendssl: require(
+          '../../public/images/docs/security/https/backend ssl blur.png'),
+      certificate: require('../../public/images/docs/security/https/인증서.PNG'),
     },
-    closedbackend:{
-      apikey: require('../../public/images/docs/security/closedbackend/apikeyblur.PNG'),
-      iptable: require('../../public/images/docs/security/closedbackend/iptable.PNG'),
-      wafinfo: require('../../public/images/docs/security/closedbackend/WAFinfo.PNG'),
-      wafblur: require('../../public/images/docs/security/closedbackend/WAFblur.PNG'),
-      block: require('../../public/images/docs/security/closedbackend/백엔드 차단.png'),
-      notblock: require('../../public/images/docs/security/closedbackend/백엔드차단안하면.PNG'),
-      iprules: require('../../public/images/docs/security/closedbackend/수신규칙.PNG'),
-      websocketproxy: require('../../public/images/docs/security/closedbackend/웹소켓 프록싱.png'),
-      websocketproxylog: require('../../public/images/docs/security/closedbackend/웹소켓프록싱로그.PNG'),
+    closedbackend: {
+      apikey: require(
+          '../../public/images/docs/security/closedbackend/apikeyblur.PNG'),
+      iptable: require(
+          '../../public/images/docs/security/closedbackend/iptable.PNG'),
+      wafinfo: require(
+          '../../public/images/docs/security/closedbackend/WAFinfo.PNG'),
+      wafblur: require(
+          '../../public/images/docs/security/closedbackend/WAFblur.PNG'),
+      block: require(
+          '../../public/images/docs/security/closedbackend/백엔드 차단.png'),
+      notblock: require(
+          '../../public/images/docs/security/closedbackend/백엔드차단안하면.PNG'),
+      iprules: require(
+          '../../public/images/docs/security/closedbackend/수신규칙.PNG'),
+      websocketproxy: require(
+          '../../public/images/docs/security/closedbackend/웹소켓 프록싱.png'),
+      websocketproxylog: require(
+          '../../public/images/docs/security/closedbackend/웹소켓프록싱로그.PNG'),
     },
     websocketproxy: {
-      before: require('../../public/images/docs/security/websocketproxy/웹소켓프록싱이전.png'),
-      after: require('../../public/images/docs/security/websocketproxy/웹소켓프록싱이후.PNG'),
-      log: require('../../public/images/docs/security/websocketproxy/웹소켓프록싱로그.PNG'),
+      before: require(
+          '../../public/images/docs/security/websocketproxy/웹소켓프록싱이전.png'),
+      after: require(
+          '../../public/images/docs/security/websocketproxy/웹소켓프록싱이후.PNG'),
+      log: require(
+          '../../public/images/docs/security/websocketproxy/웹소켓프록싱로그.PNG'),
     }
   },
   architecture: {
     image: require('../../public/images/docs/architecture.png'),
     diagram: require('../../public/images/docs/architecture-diagram.svg'),
   },
-  font:{
+  font: {
     uxBefore: require('../../public/images/docs/fontUXbefore.gif'),
     uxAfter: require('../../public/images/docs/fontUXafter.gif'),
     optimizeBefore: require('../../public/images/docs/fontOptimizeBefore.PNG'),
     optimizeAfter: require('../../public/images/docs/fontOptimizeAfter.PNG'),
   },
-  reqOptimize:{
+  reqOptimize: {
     before: require('../../public/images/docs/issue/before.gif'),
     after: require('../../public/images/docs/issue/after.gif'),
   },
   dberror: require('../../public/images/docs/issue/global_error_handler.PNG'),
   platedoc: require('../../public/images/docs/issue/immutablePlate.png'),
-  staticsrc:{
+  lighthouse: require('../../public/images/docs/lighthouse.PNG'),
+  staticsrc: {
     fail: require('../../public/images/docs/issue/static-src-fail.PNG'),
     faillog: require('../../public/images/docs/issue/static-src-fail-log.png'),
   },
   /*계정 기능*/
-  account:{
-  signup:{
-    test: require('../../public/images/docs/account/signup/testsignup.gif'),
-    email: require('../../public/images/docs/account/signup/email.gif'),
-  },
-  login:{
-    ui: require('../../public/images/docs/account/login/login1.PNG'),
-    socialGoogle: require('../../public/images/docs/account/login/sociallogin.gif'),
-    socialKakao: require('../../public/images/docs/account/login/카카오로그인.gif'),
-    socialNaver: require('../../public/images/docs/account/login/네이버로그인.gif'),
-  },
-  forgot:{
-    id: require('../../public/images/docs/account/forgot/forgotid.gif'),
-    pw: require('../../public/images/docs/account/forgot/forgotpw.gif'),
-  },
-  profile: require('../../public/images/docs/account/profile.gif'),
-  deleteAccount: require('../../public/images/docs/account/delete.gif'),
-  socialDeleteLink: require('../../public/images/docs/account/탈퇴연동.gif'),
+  account: {
+    signup: {
+      test: require('../../public/images/docs/account/signup/testsignup.gif'),
+      email: require('../../public/images/docs/account/signup/email.gif'),
+    },
+    login: {
+      ui: require('../../public/images/docs/account/login/login1.PNG'),
+      socialGoogle: require(
+          '../../public/images/docs/account/login/sociallogin.gif'),
+      socialKakao: require('../../public/images/docs/account/login/카카오로그인.gif'),
+      socialNaver: require('../../public/images/docs/account/login/네이버로그인.gif'),
+    },
+    forgot: {
+      id: require('../../public/images/docs/account/forgot/forgotid.gif'),
+      pw: require('../../public/images/docs/account/forgot/forgotpw.gif'),
+    },
+    profile: require('../../public/images/docs/account/profile.gif'),
+    deleteAccount: require('../../public/images/docs/account/delete.gif'),
+    socialDeleteLink: require('../../public/images/docs/account/탈퇴연동.gif'),
   },
   /*노트 기능*/
-  notes:{
-    filter:{
+  notes: {
+    filter: {
       category: require('../../public/images/docs/notes/카테고리.gif'),
       tag: require('../../public/images/docs/notes/태그.gif'),
       compositeFilter: require('../../public/images/docs/notes/필터복합.gif'),
